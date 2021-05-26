248683980a069c3f588e878bece703a8d8ec68e5
joon-so <joonso16@gmail.com>
2021-05-26 17:33:13 +0900

    줄무늬, 마름모, 사각형 패턴

--- GLSLBase/Shaders/Texture.fs
+++ GLSLBase/Shaders/Texture.fs
@@ -6,8 +6,19 @@ uniform sampler2D u_TexSampler;
 
 in vec2 v_TexPos;
 
+const float PI = 3.141592;
+
 void main()
 {
 	//FragColor = vec4(1);//texture(u_TexSampler, v_TexPos);
-	FragColor = vec4(v_TexPos,0,1);
+	vec2 xy = v_TexPos;
+	float maskY = xy.y * 2 * PI * 8; //0~2PI
+	float sinValueY = sin(maskY);
+
+	float maskX = xy.x * 2 * PI * 8; //0~2PI
+	float sinValueX = sin(maskX);
+
+	//FragColor = vec4(sinValueY); //줄무의 패턴
+	//FragColor = vec4(ceil(sinValueX + sinValueY)); //마름모 패턴
+	FragColor = vec4(max(sinValueX, sinValueY)); //감옥패턴
 }
