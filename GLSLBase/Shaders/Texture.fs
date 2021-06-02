eb0551799947c7fc53f07ad958d15cfbc90d69e4
joon-so <joonso16@gmail.com>
2021-06-02 17:14:09 +0900

    Texture +

--- GLSLBase/Shaders/Texture.fs
+++ GLSLBase/Shaders/Texture.fs
@@ -8,7 +8,26 @@ in vec2 v_TexPos;
 
 const float PI = 3.141592;
 
+vec4 p1()
+{
+	vec2 newTex = v_TexPos;
+	newTex.y = abs(newTex.y - 0.5) * 2;
+	vec4 returnColor = texture(u_TexSampler, newTex);
+
+	return returnColor;
+}
+
+vec4 p2()
+{
+	vec2 newTex = v_TexPos;
+	newTex.x = fract(newTex.x * 3.0);
+	newTex.y = floor(v_TexPos.x*3.0) / 3.0 + newTex.y/3.0;
+
+	vec4 returnColor = texture(u_TexSampler, newTex);
+	return returnColor;
+}
+
 void main()
 {
-	FragColor = texture(u_TexSampler, v_TexPos);
+	FragColor = p2();
 }
